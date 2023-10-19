d2bb45527613cc63ff507d183a91c3928217ba1f
Todelas13 <120015178+Todelas13@users.noreply.github.com>
2023-10-19 17:02:58 -0300

    ++

--- webapp/assets/calculadora/calculadora.js
+++ webapp/assets/calculadora/calculadora.js
@@ -1,3 +1,36 @@
 $(function(){
     $("#header").load("/webapp/assets/header/header.html");
 });
+
+//calcular valor final de um rendimento
+
+function calcular(){
+    let valorInicial = document.getElementById("valorInicial").value;
+    let rendimento = document.getElementById("rendimento").value;
+    let meses = document.getElementById("meses").value;
+    let valorFinal = valorInicial * Math.pow((1 + rendimento/100), meses);
+    
+    for (let i = 1; i <= meses; i++) {
+        let valorMes = valorInicial * Math.pow((1 + rendimento/100), i);
+        let mes = i;
+
+        // adicionar esse dados no calculadora.json
+        // {
+        //     "mes": 1,
+        //     "valor": 1000
+        // }
+
+
+
+
+
+
+    return document.getElementById("valorFinal").value = valorFinal.toFixed(2);
+
+}
+
+//usar a funcao calcular ao clicar botao
+
+document.getElementById("calcular").onclick = function(){
+    calcular();
+}
