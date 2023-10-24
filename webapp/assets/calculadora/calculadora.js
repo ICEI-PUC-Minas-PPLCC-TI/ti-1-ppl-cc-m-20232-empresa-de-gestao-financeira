728f2f20cdf6e0f9378eebd4573756b0dcd4d503
Todelas13 <120015178+Todelas13@users.noreply.github.com>
2023-10-24 15:02:11 -0300

    ++

--- webapp/assets/calculadora/calculadora.js
+++ webapp/assets/calculadora/calculadora.js
@@ -1,85 +1,81 @@
-$(function(){
+$(function() {
     $("#header").load("/webapp/assets/header/header.html");
 });
 
-function calcular() {
+async function calcular() {
+    try {
+        let valorInicial = parseFloat(document.getElementById("valorInicial").value);
+        let rendimento = parseFloat(document.getElementById("rendimento").value);
+        let meses = parseInt(document.getElementById("meses").value);
+        let valorFinal = valorInicial;
 
+        let data = [];
 
-    let valorInicial = parseFloat(document.getElementById("valorInicial").value);
-    let rendimento = parseFloat(document.getElementById("rendimento").value);
-    let meses = parseInt(document.getElementById("meses").value);
-    let valorFinal = valorInicial;
-    let data;  // Declare data variable here
-
-    fetch('https://api.jsonbin.io/v3/b/6532b95f54105e766fc4dfa9')
-        .then(response => response.json())
-        .then(db => {
-            data = [];  // Assign value to the data variable
-
-            for (let i = 1; i <= meses; i++) {
-                valorFinal = valorFinal * (1 + rendimento / 100);
-                let a = {
-                    mes: i,
-                    valor: parseFloat(valorFinal.toFixed(2))
-                };
-                data.push(a);
-            }
-            document.getElementById("valorFinal").value = valorFinal.toFixed(2);
-            return data;
-        })
-        .then(() => {
-            let req = new XMLHttpRequest();
-
-            req.onreadystatechange = () => {
-                if (req.readyState == XMLHttpRequest.DONE) {
-                    console.log(req.responseText);
-                }
+        for (let i = 1; i <= meses; i++) {
+            valorFinal = valorFinal * (1 + rendimento / 100);
+            let a = {
+                mes: i,
+                valor: parseFloat(valorFinal.toFixed(2))
             };
+            data.push(a);
+        }
 
-            req.open("PUT", "https://api.jsonbin.io/v3/b/6532b95f54105e766fc4dfa9", true);
-            req.setRequestHeader("Content-Type", "application/json");
-            req.setRequestHeader("X-Master-Key", "$2a$10$jyAfBTwo2U6.TwQfTEaUGebvDvlG8w0eBEect7I9cea8YAAK.Tzdy");
-            req.send(JSON.stringify({ data }));  // Use the data variable here
+        document.getElementById("valorFinal").value = valorFinal.toFixed(2);
 
-        })
-
-
-
-        // .then(response => response.json())
-        // .then(updatedData => {
-        //     console.log("Update successful:", updatedData);
-        // })
-        // .catch(error => {
-        //     console.error("Error updating data:", error);
-        // });
-
-        fetch('https://api.jsonbin.io/v3/b/6532b95f54105e766fc4dfa9')
-        .then(response => response.json())
-        .then(db => {
-            data = db.record.data;  // Assign value to the data variable
-
-            let textoHTML = ''
-            for (i=0; i < data.length; i++) {
-                let a = data[i]
-                textoHTML += `<tr><td>${a.mes}</td><td>${a.valor}</td></tr>`
-            }
-            document.getElementById('tabelaValores').innerHTML = textoHTML
-        })
+        await updateData(data);
+        await fetchDataAndPopulateTable();
+    } catch (error) {
+        console.error("Error during calculation:", error);
+    }
 }
 
+async function updateData(data) {
+    try {
+        let response = await fetch("https://api.jsonbin.io/v3/b/6532b95f54105e766fc4dfa9", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Master-Key": "$2a$10$jyAfBTwo2U6.TwQfTEaUGebvDvlG8w0eBEect7I9cea8YAAK.Tzdy"
+            },
+            body: JSON.stringify({ data })
+        });
 
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
+        console.log("Data updated successfully");
+    } catch (error) {
+        console.error("Error updating data:", error);
+    }
+}
 
-//usar a funcao calcular ao clicar botao
+async function fetchDataAndPopulateTable() {
+    try {
+        let response = await fetch("https://api.jsonbin.io/v3/b/6532b95f54105e766fc4dfa9");
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let db = await response.json();
+        let data = db.record.data;
 
-document.getElementById("calcular").onclick = function(){
+        let textoHTML = '';
+        for (let i = 0; i < data.length; i++) {
+            let a = data[i];
+            textoHTML += `<tr><td>${a.mes}</td><td>${a.valor}</td></tr>`;
+        }
+        document.getElementById('tabelaValores').innerHTML = textoHTML;
+        console.log("Table updated successfully");
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+document.getElementById("calcular").onclick = function() {
     limparTabela();
     calcular();
-}
-
-
-// limpar innerHTML da tabela
+};
 
 function limparTabela() {
-    document.getElementById('tabelaValores').innerHTML = ''
+    document.getElementById('tabelaValores').innerHTML = '';
 }
